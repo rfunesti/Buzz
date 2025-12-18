48c1fe5c0d016d112cb4bf8cc6de81bfda6ac547
Robert H. Funesti <rfunesti@essextech.org>
2025-12-18 07:35:39 -0500

    1218-2

--- Assets/Scripts/MobileInput.cs
+++ Assets/Scripts/MobileInput.cs
@@ -5,16 +5,10 @@ public class MobileInput : MonoBehaviour
     public static MobileInput I;   // quick singleton access (optional)
     void Awake() => I = this;
 
-    public bool leftHeld;
-    public bool rightHeld;
-
     // “Tap” actions (set true briefly)
     public bool jumpPressed;
     public bool firePressed;
-    public void LeftDown() => leftHeld = true;
-    public void LeftUp() => leftHeld = false;
-    public void RightDown() => rightHeld = true;
-    public void RightUp() => rightHeld = false;
+
     public void JumpTap() => jumpPressed = true;
     public void FireTap() => firePressed = true;
 
